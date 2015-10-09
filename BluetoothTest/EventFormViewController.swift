f7888fa6e8c3809a302d240799fba706840efb23
Julian Ferdman <julianferdman@gmail.com>
2015-10-09 16:35:55 -0400

    new fields, some cleanup

--- BluetoothTest/EventFormViewController.swift
+++ BluetoothTest/EventFormViewController.swift
@@ -14,17 +14,18 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
     
     var eventTitle = UITextField()
     var eventAddress = UITextField() //Thought is to change all address fields to a single Maps Query
-    var matchingItems: [MKMapItem] = [MKMapItem]()
     var eventStart = UITextField()
     var eventEnd = UITextField()
+    var eventCompany = UITextField()
+    var Description = UITextField()
     
     let dateFormat: NSDateFormatter = NSDateFormatter()
     let startDatePicker: UIDatePicker = UIDatePicker()
     let endDatePicker: UIDatePicker = UIDatePicker()
     var fontAwesomeIcon: UILabel!
-    let ThumbsUp = "\u{f164}"
     
-    //Company Name, Image, Description, Max Participants,
+    //To do: Image, Description, Max Participants
+    //To do: Fix Address field
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,23 +41,17 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
         
         let label: UILabel = UILabel(frame: CGRectMake(0, 0, self.view.frame.size.width, 120))
         label.textAlignment = NSTextAlignment.Center
-        label.text = "Create Event"
-        label.font = UIFont(name: "Courier New", size: 30)
+        label.text = "\u{f271} Create Event"
+        label.font = UIFont(name: "FontAwesome", size: 30)
         label.backgroundColor = UIColor(red: 0, green: 74, blue: 255, alpha: 1.0)
         self.view.addSubview(label)
         
         view.backgroundColor = UIColor(red: 0.16, green: 0.19, blue: 0.22, alpha: 1.0)
         
-        //navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
-        //navigationController?.navigationBar.shadowImage = UIImage()
-        //navigationController?.navigationBar.translucent = true
-        //navigationController?.view.backgroundColor = UIColor.clearColor()
-        //UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
-        
-        //let cohesveBlue = UIColor(red: 0.0, green: 0.74, blue: 1.0, alpha: 1.0).CGColor
         
         
-        let labelItems: UILabel = UILabel(frame: CGRectMake(0, 200, self.view.frame.size.width, 10))
+        //Unnecessary Label
+        let labelItems: UILabel = UILabel(frame: CGRectMake(0, 200, self.view.frame.size.width, 13))
         labelItems.textAlignment = NSTextAlignment.Center
         labelItems.text = "Fields"
         labelItems.font = UIFont(name: "FontAwesome", size: 12)
@@ -65,6 +60,7 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
         self.view.addSubview(labelItems)
         
         
+        //eventTitle Field
         view.addSubview(eventTitle)
         eventTitle.delegate=self
         eventTitle.translatesAutoresizingMaskIntoConstraints = false
@@ -81,6 +77,42 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
         view.addConstraint(NSLayoutConstraint(item: eventTitle, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterY, multiplier: 1.0, constant: -100)) //Tell field to center on y-axis
         view.addConstraint(NSLayoutConstraint(item: eventTitle, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
         view.addConstraint(NSLayoutConstraint(item: eventTitle, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
+        
+        
+        //eventAddress Field
+        view.addSubview(eventAddress)
+        //eventAddress.delegate=self
+        eventAddress.backgroundColor = UIColor.whiteColor()
+        eventAddress.layer.borderColor = UIColor(red:0/255.0, green:188.7/255.0, blue:255/255.0, alpha: 1.0).CGColor
+        eventAddress.layer.borderWidth = 1.5
+        eventAddress.attributedPlaceholder = NSAttributedString(string:"\u{f124} Location",
+            attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+        eventAddress.font = UIFont(name: "FontAwesome", size: 12)
+        eventAddress.layer.sublayerTransform = CATransform3DMakeTranslation(10, 000, 0)
+        eventAddress.translatesAutoresizingMaskIntoConstraints = false
+        eventAddress.layer.cornerRadius = 10
+        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)) //Tell field to center on x-axis
+        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: eventTitle, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10)) //Tell field to center on y-axis
+        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
+        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
+        
+        //eventCompany Field
+        view.addSubview(eventCompany)
+        //eventAddress.delegate=self
+        eventCompany.backgroundColor = UIColor.whiteColor()
+        eventCompany.layer.borderColor = UIColor(red:0/255.0, green:188.7/255.0, blue:255/255.0, alpha: 1.0).CGColor
+        eventCompany.layer.borderWidth = 1.5
+        eventCompany.attributedPlaceholder = NSAttributedString(string:"\u{f0f2} Company Name",
+            attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+        eventCompany.font = UIFont(name: "FontAwesome", size: 12)
+        eventCompany.layer.sublayerTransform = CATransform3DMakeTranslation(10, 000, 0)
+        eventCompany.translatesAutoresizingMaskIntoConstraints = false
+        eventCompany.layer.cornerRadius = 10
+        view.addConstraint(NSLayoutConstraint(item: eventCompany, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)) //Tell field to center on x-axis
+        view.addConstraint(NSLayoutConstraint(item: eventCompany, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: eventAddress, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10)) //Tell field to center on y-axis
+        view.addConstraint(NSLayoutConstraint(item: eventCompany, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
+        view.addConstraint(NSLayoutConstraint(item: eventCompany, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
+        
         
         // Setup Start date picker
         dateFormat.dateStyle = NSDateFormatterStyle.ShortStyle
@@ -110,7 +142,7 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
         eventStart.layer.sublayerTransform = CATransform3DMakeTranslation(10, 000, 0)
         eventStart.layer.cornerRadius = 10
         view.addConstraint(NSLayoutConstraint(item: eventStart, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)) //Tell field to center on x-axis
-        view.addConstraint(NSLayoutConstraint(item: eventStart, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: eventTitle, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10)) //Tell field to center on y-axis
+        view.addConstraint(NSLayoutConstraint(item: eventStart, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: eventCompany, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10)) //Tell field to center on y-axis
         view.addConstraint(NSLayoutConstraint(item: eventStart, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
         view.addConstraint(NSLayoutConstraint(item: eventStart, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
         
@@ -132,23 +164,6 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
         view.addConstraint(NSLayoutConstraint(item: eventEnd, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
         view.addConstraint(NSLayoutConstraint(item: eventEnd, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
         
-        
-        //eventAddress Field
-        view.addSubview(eventAddress)
-        //eventAddress.delegate=self
-        eventAddress.backgroundColor = UIColor.whiteColor()
-        eventAddress.layer.borderColor = UIColor(red:0/255.0, green:188.7/255.0, blue:255/255.0, alpha: 1.0).CGColor
-        eventAddress.layer.borderWidth = 1.5
-        eventAddress.attributedPlaceholder = NSAttributedString(string:"\u{f124} Location",
-            attributes:[NSForegroundColorAttributeName: UIColor.lightGrayColor()])
-        eventAddress.font = UIFont(name: "FontAwesome", size: 12)
-        eventAddress.layer.sublayerTransform = CATransform3DMakeTranslation(10, 000, 0)
-        eventAddress.translatesAutoresizingMaskIntoConstraints = false
-        eventAddress.layer.cornerRadius = 10
-        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0)) //Tell field to center on x-axis
-        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: eventEnd, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 10)) //Tell field to center on y-axis
-        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: .Height, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 30))
-        view.addConstraint(NSLayoutConstraint(item: eventAddress, attribute: .Width, relatedBy: .Equal, toItem: nil, attribute: .NotAnAttribute, multiplier: 1.0, constant: 200))
         
         
         //Keyboard Fix
@@ -203,7 +218,7 @@ class EventFormViewController: UIViewController, UITextFieldDelegate {
     
     func performSearch() {
         
-        matchingItems.removeAll()
+        //matchingItems.removeAll()
         let request = MKLocalSearchRequest()
         request.naturalLanguageQuery = eventAddress.text
         //request.region = mapView.region
